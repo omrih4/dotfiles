dfbe10c3f13327bc503f0ebcc147b6b875e56c4d
Omri H <omrih.e@protonmail.com>
2026-03-16 14:43:36 +0200

    vim: system clipboard, git integration, undo file

--- modules/vim/core.nix
+++ modules/vim/core.nix
@@ -1,12 +1,15 @@
 {
   programs.nvf.settings.vim = {
+    undoFile.enable = true;
     clipboard = {
       enable = true;
       providers.wl-copy.enable = true;
+      registers = "unnamedplus";
     };
 
     utility = {
       sleuth.enable = true;
     };
+    git.enable = true;
   };
 }
